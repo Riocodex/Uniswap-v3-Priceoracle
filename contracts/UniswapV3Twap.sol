cbcd2184f489b048beb90a7cda2f33e702e1ea68
Riocodex <onwukachibike@gmail.com>
2023-06-15 16:49:48 +0100

    updated

--- contracts/UniswapV3Twap.sol
+++ contracts/UniswapV3Twap.sol
@@ -1,38 +1,49 @@
-//SPDX-License-Identifier:MIT
+//SPDX-License-Identifier: MIT
 pragma solidity 0.7.6;
 
-import "@uniswap/v3-core/contracts/interfaces/callback/IUniswapV3Factory.sol";
-import "@uniswap/lib/contracts/libraries/OracleLibrary.sol";
+import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Factory.sol";
+import "@uniswap/v3-periphery/contracts/libraries/OracleLibrary.sol";
 
 contract UniswapV3Twap {
     address public immutable token0;
     address public immutable token1;
     address public immutable pool;
 
-
-    constructor(address _factory,address _token0, address _token1,uint24 fee) {
+    constructor(
+        address _factory,
+        address _token0,
+        address _token1,
+        uint24 _fee
+    ) {
         token0 = _token0;
         token1 = _token1;
 
         address _pool = IUniswapV3Factory(_factory).getPool(
-            _token0, _token1, _fee
+            _token0,
+            _token1,
+            _fee
         );
-        require(_pool != address(0), "pool doesnt exist");
+        require(_pool != address(0), "pool doesn't exist");
+
         pool = _pool;
-    } 
+    }
 
     function estimateAmountOut(
         address tokenIn,
         uint128 amountIn,
-        uint132 secondsAgo
-    )external view returns (uint amountOut) {
+        uint32 secondsAgo
+    ) external view returns (uint amountOut) {
         require(tokenIn == token0 || tokenIn == token1, "invalid token");
+
         address tokenOut = tokenIn == token0 ? token1 : token0;
+
+        // (int24 tick, ) = OracleLibrary.consult(pool, secondsAgo);
+
         // Code copied from OracleLibrary.sol, consult()
         uint32[] memory secondsAgos = new uint32[](2);
         secondsAgos[0] = secondsAgo;
         secondsAgos[1] = 0;
- 
+
         // int56 since tick * time = int24 * uint32
         // 56 = 24 + 32
         (int56[] memory tickCumulatives, ) = IUniswapV3Pool(pool).observe(
@@ -60,9 +71,12 @@ contract UniswapV3Twap {
         ) {
             tick--;
         }
-        (int24 tick, ) = OracleLibrary.consult(pool, secondsAgo);
+
         amountOut = OracleLibrary.getQuoteAtTick(
-            tick, amountIn, tokenIn, tokenOut
+            tick,
+            amountIn,
+            tokenIn,
+            tokenOut
         );
     }
 }
